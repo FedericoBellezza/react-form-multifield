80c90bafea6bbc47da02929d9467c0de6aa1b1a1
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-12-04 19:00:36 +0100

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,34 @@ import Header from "./assets/components/Header";
 function App() {
   const [titles, setTitles] = useState([
     {
-      title: "Titolo 1",
-      img: "",
-      content: "",
-      category: "",
+      title: "Il mio cane di nome Doggo",
+      img: "https://pbs.twimg.com/profile_images/1371936477848944649/C1LgElS5_400x400.jpg",
+      content: "Bellissima foto di un cane.",
+      category: "Animali",
+      isPublic: "true",
+    },
+    {
+      title: "Occhio ai colori",
+      img: "https://media.istockphoto.com/id/814423752/it/foto/occhio-di-modello-con-make-up-artistico-colorato-primo.jpg?s=612x612&w=0&k=20&c=OFhqyikwC47uoihOoQdQ4hnyVPo3qhyFPWldANTEjzk=",
+      content: "Occhio per occhio.",
+      category: "Arte",
+      isPublic: "true",
+    },
+    {
+      title: "Verso l'infinito ed oltre",
+      img: "https://cdn.pixabay.com/photo/2018/02/27/18/17/road-3186188_640.jpg",
+      content: "Chissà dove porterà questa strada..",
+      category: "Natura",
       isPublic: "true",
     },
   ]);
+
   const [inputValue, setInputValue] = useState("");
+  const [img, setImg] = useState("");
+  const [content, setContent] = useState("");
+  const [category, setCategory] = useState("Casual");
+  const [isPublic, setIsPublic] = useState("true");
+  const [title, setTitle] = useState("");
   let newTitles = [...titles];
 
   // - Funzione per eliminare un post
@@ -26,96 +46,153 @@ function App() {
     }
   };
 
-  // - Al cambio di stato dell'input del titolo
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+  // - Alla modifica dell'input
+  const handleFormChange = (e) => {
+    if (e.target.name === "title") setTitle(e.target.value);
+    if (e.target.name === "img") setImg(e.target.value);
+    if (e.target.name === "content") setContent(e.target.value);
+    if (e.target.name === "category") setCategory(e.target.value);
+    if (e.target.name === "isPublic") setIsPublic(e.target.value);
   };
 
   // - All'invio del form
   const handleFormSubmit = (e) => {
     event.preventDefault();
-    if (!inputValue) return;
-    console.log("Aggiunto il titolo: " + inputValue);
+    if (!title) return;
+    if (!img) return;
+    if (!content) return;
+    setInputValue({
+      title,
+      img,
+      content,
+      category,
+      isPublic,
+    });
+
+    console.log("Aggiunto il titolo: " + title);
     newTitles = [
       ...titles,
       {
-        title: inputValue,
+        title,
+        img,
+        content,
+        category,
+        isPublic,
       },
     ];
     setTitles(newTitles);
-    setInputValue("");
+    console.log(newTitles);
   };
   return (
     <>
       <div className="container">
         <Header />
 
-        <form onSubmit={handleFormSubmit} className="row g-3 input-group mt-5 ">
+        <form
+          onChange={handleFormChange}
+          onSubmit={handleFormSubmit}
+          className="row g-3 input-group mt-5"
+        >
           {/* Titolo */}
           <div className="col-6">
             <input
-              onChange={handleInputChange}
+              onChange={handleFormChange}
+              name="title"
               type="text"
-              className="form-control"
-              value={inputValue}
+              className="form-title form-control"
+              value={title}
               placeholder="Titolo"
             />
           </div>
           {/* Immagine */}
           <div className="col-6">
             <input
-              onChange={handleInputChange}
+              onChange={handleFormChange}
+              name="img"
               type="select"
-              className="form-control"
-              value={inputValue}
+              className="form-img form-control"
+              value={img}
               placeholder="Immagine"
             />
           </div>
           {/* Categoria */}
           <div className="col-6">
-            <select class="form-select" aria-label="Default select example">
+            <select
+              onChange={handleFormChange}
+              name="category"
+              className="form-category form-select"
+              aria-label="Default select example"
+            >
               <option selected>Categoria</option>
-              <option>Cat 1</option>
-              <option>Cat 2</option>
-              <option>Cat 3</option>
+              <option>Arte</option>
+              <option>Animali</option>
+              <option>Tecnologia</option>
+              <option>Natura</option>
+              <option>Musica</option>
             </select>
           </div>
           {/* Pubblico */}
           <div className="col-6">
-            <select class="form-select" aria-label="Default select example">
-              <option selected>Pubblico</option>
-              <option>Privato</option>
+            <select
+              onChange={handleFormChange}
+              name="isPublic"
+              className="form-ispublic form-select"
+              aria-label="Default select example"
+            >
+              <option value={true} selected>
+                Pubblico
+              </option>
+              <option value={false}>Privato</option>
             </select>
           </div>
           <div className="col-12">
             <textarea
-              onChange={handleInputChange}
-              className="form-control textarea"
-              value={inputValue}
+              onChange={handleFormChange}
+              name="content"
+              className="form-description  form-control textarea"
+              value={content}
               placeholder="Descrizione"
             />
           </div>
-          <button className="btn btn-outline-secondary">Aggiungi</button>
+          <button className="btn rounded btn-outline-secondary">
+            Aggiungi
+          </button>
         </form>
         <hr />
-        <div className="row row-cols-3 g-3 ">
-          {/* Stampo una card per ogni elemento dell'array "titles" */}
+        <div className="row row-cols-3 g-3 mb-5 ">
+          {/* Stampo una card per ogni elemento dell'array "titles" dinamicamente */}
           {titles.map((element) => {
             return (
               <>
                 <div key={element} className="col">
-                  <div className="default-card justify-content-between d-flex">
-                    <div className="card-title">{element.title}</div>
-                    <div>
-                      <button className="btn btn-primary  me-3 ">
-                        <i className=" fa-solid fa-pen"></i>
-                      </button>
-                      <button
-                        className="btn btn-primary  "
-                        onClick={() => deleteTitle(element)}
-                      >
-                        <i className=" fa-solid fa-trash"></i>
-                      </button>
+                  <div className="card">
+                    <div className="img-container">
+                      <img
+                        src={element.img}
+                        className="card-img-top"
+                        alt="..."
+                      />
+                    </div>
+
+                    <div className="card-body d-flex align-items-stretch justify-content-between flex-column">
+                      <h5 className="card-title">{element.title}</h5>
+                      <p className="card-text">{element.content}</p>
+                      <div className="d-flex flex-row  justify-content-between">
+                        <div href="#" className="badge text-bg-primary">
+                          {element.category}
+                        </div>
+                        <div>
+                          <button className="btn btn-primary  me-3 ">
+                            <i className=" fa-solid fa-pen"></i>
+                          </button>
+                          <button
+                            className="btn btn-primary  "
+                            onClick={() => deleteTitle(element)}
+                          >
+                            <i className=" fa-solid fa-trash"></i>
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
